f72b0bc5a366105c566642a183b9d26a1e91785b
Luís Guilherme Theóphilo Malveira <97563186+LuisGuilhermeMalveira@users.noreply.github.com>
2022-10-28 03:23:54 -0300

    gravando o input no console.log e validação

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,5 @@
+//Puxando a data do sistema
+
 let datahj = document.querySelector(".datahj");
 
 datahj.innerHTML = dataAtualFormatada();
@@ -10,4 +12,29 @@ function dataAtualFormatada() {
 		mesF = mes.length == 1 ? "0" + mes : mes,
 		anoF = data.getFullYear();
 	return diaF + "/" + mesF + "/" + anoF;
+}
+
+//variaveis para fazer a tarefa
+let btnAdicionar = document.querySelector(".blocoAdd img");
+let inputTarefa = document.querySelector(".blocoAdd input");
+let formAdd = document.querySelector("#formAdd");
+
+btnAdicionar.addEventListener("click", function (event) {
+	event.preventDefault();
+	validandoInput();
+	addTarefa();
+});
+
+//validando para ver se o usuário digitou algo.
+function validandoInput() {
+	if (formAdd.tarefaAdd.value != 0) {
+		return;
+	} else {
+		console.log("mensagem de erro pq o usuário não colocou nada");
+	}
+}
+
+//add o HTML
+function addTarefa() {
+	console.log(formAdd.tarefaAdd.value);
 }
